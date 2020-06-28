e6c7d27bc961b2c64867355458f86b43638e698f
Kenneth Silvere <kennethsilvere93@gmail.com>
2020-06-28 18:09:52 +0530

    Build bare bar chart with dummy data

--- app.js
+++ app.js
@@ -4,3 +4,30 @@ const DUMMY_DATA = [
     { id: 'd3', region: 'China', value: 11 },
     { id: 'd4', region: 'Germany', value: 6 },
   ];
+
+const MARGINS = { top: 20, bottom: 20 };
+const CHART_WIDTH = 600;
+const CHART_HEIGHT = 400;
+
+const chartContainer = d3.select('#chart')
+                         .select('svg')
+                         .attr('width', CHART_WIDTH)
+                         .attr('height', CHART_HEIGHT + MARGINS.top + MARGINS.bottom);
+
+const chart = chartContainer.append('g');
+
+const xScale = d3.scaleBand().rangeRound([0, CHART_WIDTH]).padding(0.1);
+const yScale = d3.scaleLinear().range([CHART_HEIGHT, 0]);
+
+xScale.domain(DUMMY_DATA.map(d => d.region));
+yScale.domain([0, d3.max(DUMMY_DATA, d => d.value) + 3]);
+
+const bars = chart.selectAll('.bar')
+                  .data(DUMMY_DATA)
+                  .enter()
+                  .append('rect')
+                  .classed('bar', true)
+                  .attr('width', xScale.bandwidth())
+                  .attr('height', d => (CHART_HEIGHT - yScale(d.value)))
+                  .attr('x', d => xScale(d.region))
+                  .attr('y', d => yScale(d.value));
