8d44518ca125bf2899a349f4b1974f10b63d4df5
Kenneth Silvere <kennethsilvere93@gmail.com>
2020-06-28 17:53:20 +0530

    Starting code template provided by Academind

---
+++ app.js
@@ -0,0 +1,6 @@
+const DUMMY_DATA = [
+    { id: 'd1', region: 'USA', value: 10 },
+    { id: 'd2', region: 'India', value: 12 },
+    { id: 'd3', region: 'China', value: 11 },
+    { id: 'd4', region: 'Germany', value: 6 },
+  ];
